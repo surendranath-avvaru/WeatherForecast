1ef30f500d586f6260a65a9a07dab396cf784152
surendranath-avvaru <surendranath.avvaru@aspiresys.com>
2018-04-13 00:29:49 +0530

    Integrate google maps

--- src/components/chart.js
+++ src/components/chart.js
@@ -2,19 +2,30 @@ import React from 'react';
 import _ from 'lodash';
 import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines';
 
+function low(data) {
+	return (_.min(data));
+}
 
 function average(data) {
 	return _.round(_.sum(data)/data.length);
 }
 
+function high(data) {
+	return (_.max(data));
+}
+
 export default (props) => {
 	return (
 		<div>
-			<Sparklines data={props.data} height={120} width={180}>
+			<Sparklines data={props.data} height={100} width={150}>
 				<SparklinesReferenceLine type="avg" />
 				<SparklinesLine color={props.color} />
 			</Sparklines>
-			<div>{average(props.data)} {props.units}</div>
+			<div className="unitLabels">
+			<div><b>Min:</b> {low(props.data)} {props.units}</div>
+			<div><b>Avg:</b> {average(props.data)} {props.units}</div>
+			<div><b>Max:</b> {high(props.data)} {props.units}</div>
+			</div>
 		</div>
 	);
 }
