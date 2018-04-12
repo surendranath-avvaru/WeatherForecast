b49524526068fe9ecd5b7b5c753bc9468a1fb452
surendranath-avvaru <surendranath.avvaru@aspiresys.com>
2018-04-12 19:31:50 +0530

    Add alert when city not found

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -4,6 +4,7 @@ export default function(state = [], action) {
 	switch (action.type) {
 	case FETCH_WEATHER:
 		if (!action.payload.data) {
+			alert("City not found!");
 			return state;
 		}
 		/*return state.concat([action.payload.data]);*/
