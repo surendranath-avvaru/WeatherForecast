079dd11011e9b53c2fb125e127d6991ec8baa533
surendranath-avvaru <surendranath.avvaru@aspiresys.com>
2018-05-03 16:23:54 +0530

    Add next & prev page links

--- src/containers/users/user_list_container.js
+++ src/containers/users/user_list_container.js
@@ -15,26 +15,42 @@ class UserList extends Component {
 	constructor(props) {
 		super(props);
 		this.getSelectedPageUsers = this.getSelectedPageUsers.bind(this);
+		this.getPreviousPageUsers = this.getPreviousPageUsers.bind(this);
+		this.getNextPageUsers = this.getNextPageUsers.bind(this);
 	}
 
 	componentDidMount() {
 		this.props.fetchUsers(this.props.start_page);
 	}
 	
-	getSelectedPageUsers(page) {
-		this.props.fetchUsers(Number(page.target.innerText));
+	getSelectedPageUsers(e) {
+		this.props.fetchUsers(Number(e.target.innerText));
+	}
+
+	getPreviousPageUsers(e, value) {
+		if(value != null) {
+			this.props.fetchUsers(value);
+		}
+	}
+
+	getNextPageUsers(e, value) {
+		if(value != null) {
+			this.props.fetchUsers(value);
+		}
 	}
 
 	render() {
 	    const per_page = 10;
 	    const pages = Math.ceil(this.props.users_count / per_page);
 	    const current_page = this.props.start_page;
+	    const previous_page = this.props.previous_page;
+	    const next_page = this.props.next_page;
 
 	    let start_count = 0;
 	    let items = [];
 	    for (let number = 1; number <= pages; number++) {
 		  items.push(
-		    <Pagination.Item onClick={(number)=>this.getSelectedPageUsers(number)} key={number} active={number === current_page}>{number}</Pagination.Item>
+		    <Pagination.Item onClick={(e)=>this.getSelectedPageUsers(e)} key={number} active={number === current_page}>{number}</Pagination.Item>
 		  );
 		}
 
@@ -55,9 +71,9 @@ class UserList extends Component {
 				</table>
 
 				<Pagination>
-				  <Pagination.Prev />
+				  <Pagination.Prev onClick={(e)=>this.getPreviousPageUsers(e, this.props.previous_page)}/>
 				  {items}
-				  <Pagination.Next />
+				  <Pagination.Next onClick={(e)=>this.getNextPageUsers(e, this.props.next_page)}/>
 				</Pagination>
 
 			</div>
@@ -69,8 +85,8 @@ class UserList extends Component {
 function mapStateToProps(state) {
 	return {
 			users: _.mapKeys(state.users.results, 'id'),
-			next_page: state.users.next,
-			previous_page: state.users.previous,
+			next_page: state.users.next != null ? Number(state.users.next.split("?")[1].match(/\d+/g)[0]) : null,
+			previous_page: state.users.previous != null ? (state.users.previous.split("?")[1] ? Number(state.users.previous.split("?")[1].match(/\d+/g)[0]) : 1) : null,
 			users_count: state.users.count,
 			start_page: state.users.next != null ? Number(state.users.next.split("?")[1].match(/\d+/g)[0])-1 : (state.users.previous != null ? (state.users.previous.split("?")[1] ? Number(state.users.previous.split("?")[1].match(/\d+/g)[0])+1 : 2):1)
 		}
